aefa361fb18425b8648db1003c3d62630acc8d23
dotneB <benoit@tioneb.ca>
2014-09-20 13:49:19 -0400

    Problems with parsing the description of The YeTee feed. Moving to a cheap regex to extract img src

--- app/controllers/latest_controller.rb
+++ app/controllers/latest_controller.rb
@@ -186,9 +186,10 @@ class LatestController < ApplicationController
         shirtName = entry.xpath("title").first.content
         shirtName = shirtName.split(" by ").first || shirtName
         shirtURL = "http://theyetee.com/"
-        entry_description = Nokogiri::HTML( entry.xpath("description").first.content )
-        entry_description.remove_namespaces!
-        shirtPhotoURL = entry_description.css("img").first["src"]
+        #entry_description = Nokogiri::HTML.fragment( entry.xpath("description").first.content )
+        #entry_description.remove_namespaces!
+        #shirtPhotoURL = entry_description.css("img").first["src"]
+        shirtPhotoURL = entry.xpath("description").first.content[ /img.*?src="(.*?)"/i,1 ].to_s
 
         shirtPubDate = entry.xpath("pubDate").first.content
 
