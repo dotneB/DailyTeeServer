d21a9603e8eeb7bf0349d6411d59c27f68a5beee
dotneB <benoit@tioneb.ca>
2014-11-27 15:07:11 -0500

    Should run on heroku

--- test/lib/shirtwoot_site_test.rb
+++ test/lib/shirtwoot_site_test.rb
@@ -12,19 +12,19 @@ class ShirtWootSiteTest < ActiveSupport::TestCase
     assert_equal( shirts.first[:shirtPhotoURL] , "http://d3gqasl9vmjfd8.cloudfront.net/d967d1d6-4f96-404a-b33b-151b48708171.png" )
   end
 
-#  test "can download" do
-#    json = ShirtWootSite::downloadFeed()
-#    assert_not_nil(json, "Unable to download ShirtWoot Feed") 
-#  end
-#
-#  test "can download and parse live" do
-#    shirts = ShirtWootSite::downloadFeedAndExtractShirts() 
-#
-#    assert_not_equal( shirts.length , 0 )
-##   shirts.each do |shirt|
-##         assert_equal( shirt[:siteId] , ShirtWootSite::SITE_ID )
-##     assert_nothing_raised( URI::InvalidURIError, URI.parse( shirt[:shirtURL] ) ) 
-##     assert_nothing_raised( URI::InvalidURIError, URI.parse( shirt[:shirtPhotoURL] ) )
-##     end
-#  end
+  test "can download" do
+    json = ShirtWootSite::downloadFeed()
+    assert_not_nil(json, "Unable to download ShirtWoot Feed") 
+  end
+
+  test "can download and parse live" do
+    shirts = ShirtWootSite::downloadFeedAndExtractShirts() 
+
+    assert_not_equal( shirts.length , 0 )
+#   shirts.each do |shirt|
+#         assert_equal( shirt[:siteId] , ShirtWootSite::SITE_ID )
+#     assert_nothing_raised( URI::InvalidURIError, URI.parse( shirt[:shirtURL] ) ) 
+#     assert_nothing_raised( URI::InvalidURIError, URI.parse( shirt[:shirtPhotoURL] ) )
+#     end
+  end
 end
